91124faa988c639848585f312475432c26573bf3
adnan hasic <hasic.adnan.ado@gmail.com>
2019-04-17 13:44:14 +0200

    Napravio metodu za kreiranje niza i za pronalazak najmanjeg u nizu

--- src/ArrayOfNumbers.java
+++ src/ArrayOfNumbers.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
 
 public class ArrayOfNumbers {
 
+	public static Scanner input = new Scanner(System.in);
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		ArrayProcessing arrayProcessing = new ArrayProcessing();
+
+		arrayProcessing.setArray(arrayProcessing.createArrayOfNumbers());
+		arrayProcessing.theSamllestNumberInAnArray();
 
 	}
 
--- src/ArrayProcessing.java
+++ src/ArrayProcessing.java
@@ -1,4 +1,49 @@
 
 public class ArrayProcessing {
 
+	private int array[];
+
+	public ArrayProcessing() {
+
+	}
+
+	public int[] getArray() {
+		return array;
+	}
+
+	public void setArray(int[] array) {
+		this.array = array;
+	}
+
+	public int[] createArrayOfNumbers() {
+
+		System.out.println("Enter the size of the array : ");
+
+		int sizeArray = ArrayOfNumbers.input.nextInt();
+
+		int array[] = new int[sizeArray];
+
+		System.out.println("Enter the elements of the array : ");
+
+		for (int i = 0; i < array.length; i++) {
+			array[i] = ArrayOfNumbers.input.nextInt();
+		}
+
+		return array;
+	}
+
+	public void theSamllestNumberInAnArray() {
+
+		int min = Integer.MAX_VALUE;
+
+		for (int i = 0; i < array.length; i++) {
+
+			if (array[i] < min) {
+				min = array[i];
+			}
+		}
+		System.out.println("The smallest number in an array is : " + min);
+
+	}
+
 }
